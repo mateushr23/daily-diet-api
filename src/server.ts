ab5a34ba3a0b568efde7df60aff7d13b9df8caf7
mateushr23 <mateushr23@gmail.com>
2025-01-30 20:59:27 -0300

    feat: post user route and types

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import fastify from "fastify"
-import { knex } from "./database"
 import { env } from "./env"
+import { usersRoutes } from "./routes/users"
 
 const app = fastify()
 
-app.get("/hello", async () => {
-  const users = await knex("users").select("*")
-  return users
-})
+app.register(usersRoutes, { prefix: "users" })
 
 app
   .listen({
