43fe3a33bfc35d306065d45be3f361423e81b6d8
mateushr23 <mateushr23@gmail.com>
2025-01-30 14:27:53 -0300

    feat: migrations and zod config

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import fastify from "fastify"
 import { knex } from "./database"
+import { env } from "./env"
 
 const app = fastify()
 
 app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*")
-
-  return tables
+  const users = await knex("users").select("*")
+  return users
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("HTTP server running")
