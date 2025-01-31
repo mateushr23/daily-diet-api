c0e706aa548c047256a6a4a81c62ee510bcc2cfc
mateushr23 <mateushr23@gmail.com>
2025-01-30 21:11:03 -0300

    feat: get users route

--- db/migrations/20250130154408_create-user.ts
+++ db/migrations/20250130154408_create-user.ts
@@ -4,10 +4,10 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("users", (table) => {
     table.uuid("id").primary()
     table.text("name").notNullable()
-    table.timestamp("created_at").defaultTo(knex.fn.now).notNullable()
+    table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable()
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable("user")
+  await knex.schema.dropTable("users")
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,6 +4,24 @@ import { knex } from "../database"
 import crypto from "node:crypto"
 
 export async function usersRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const users = await knex("users").select()
+
+    return { users }
+  })
+
+  app.get("/:id", async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const user = await knex("users").where("id", id).first()
+
+    return { user }
+  })
+
   app.post("/", async (request, reply) => {
     const createUserBodySchema = z.object({
       name: z.string(),
